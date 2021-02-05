6092a5da8e8e96574e136081883aa44a6786fa1e
Ilnura Fattakhova <paramore2101@yandex.com>
2021-02-05 18:10:22 +0500

    finished first app on Vue

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+const loader = {
+    template: '<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>',
+
+};
 const todo = new Vue({
     el: "#main",
+
     data: {
         task: "",
-        todoList: []
+        todoList: [],
+        editInput: '',
+        loading: false
+    },
+    components: {
+        loader
     },
     methods: {
         addTodo() {
            this.todoList.unshift({
                id: this.todoList.length + 1,
                title: this.task,
-               done: false
+               done: false,
+               edit: false
            })
            this.task = "" 
         },
@@ -17,20 +28,37 @@ const todo = new Vue({
             this.todoList.splice(index, 1)
         },
         fetch(){
+            this.loading = true;
             fetch('https://jsonplaceholder.typicode.com/todos')
             .then(response => response.json())
             .then(json => this.todoList=json.map(elem => {
                 return {
                     ...elem,
-                    done: false
+                    done: false,
+                    edit: false
                 }
             }))
+            this.loading = false;
         },
         doneTodo(index){
             this.todoList[index].done = !this.todoList[index].done;
-        }
+        },
+        editButton(index){
+            this.todoList[index].edit = !this.todoList[index].edit;
+            if(this.todoList[index].edit){
+                this.editInput = this.todoList[index].title;
+            }
+            if (this.todoList[index].edit === false) {
+                this.editInput = "";
+            }
+        },
+        editTodo(index){
+            this.todoList[index].title = this.editInput;
+            this.editButton(index);
+        },
     },
     mounted() {
         this.fetch()
     },
 })
+
